bd80cef9e3e608db578266644a4031d9fd087f55
Stefano C <cruz@ipk-gatersleben.de>
2020-11-13 16:23:12 +0100

    UPDADED: new module pathway.py has now methods to filter a Solution object for the Pathway group. Old class to stored data removed, now it uses the original pandas Series. Respective test added to the tests.

--- src/cobramod/pathway.py
+++ src/cobramod/pathway.py
@@ -1,23 +1,52 @@
-from contextlib import suppress
-from typing import NamedTuple
-
 from cobra.core.group import Group
 from cobra.core.solution import Solution
+from cobra.core.dictlist import DictList
 
-
-class ReactionFlux(NamedTuple):
-    reaction: str
-    flux: float
+from pandas import Series
 
 
 class Pathway(Group):
-    def __init__(self, id, name="", members=None, kind=None):
-        super().__init__(id, name, members, kind)
+    """
+    A Sub-class from the original COBRApy :func:`cobra.Group`, which inherits
+    all its attributes and adds the method solution, to get a Solution for the
+    members of this Class.
+    """
 
-    def fluxes(self):
-        pass
+    def __init__(
+        self,
+        id: str,
+        name: str = "",
+        members: DictList = None,
+        kind: str = None,
+    ):
+        super().__init__(id=id, name=name, members=members, kind=kind)
 
-    def _filter_fluxes(self, solution: Solution):
-        for member in self.members:
-            with suppress(KeyError):
-                yield member.id, solution.fluxes[member.id]
+    def _filter(self, solution: Solution, attribute: str) -> Series:
+        """
+        Filters given attribute of Solution. Only members of the Pathway will
+        appear.
+        """
+        return getattr(solution, "fluxes").filter(
+            items=[member.id for member in self.members], axis="index"
+        )
+
+    def solution(self, solution: Solution) -> Solution:
+        """
+
+        Args:
+            solution (Solution): Original COBRApy :func:`cobra.Solution` to
+                filter.
+        Returns:
+            Solution: Filtered solution with members of the Pathway class.
+        """
+        return Solution(
+            objective_value=solution.objective_value,
+            status=solution.status,
+            fluxes=self._filter(solution=solution, attribute="fluxes"),
+            reduced_costs=self._filter(
+                solution=solution, attribute="reduced_costs"
+            ),
+            shadow_prices=self._filter(
+                solution=solution, attribute="shadow_prices"
+            ),
+        )
--- tests/test_pathway.py
+++ tests/test_pathway.py
@@ -7,10 +7,18 @@ from cobramod.test import textbook_kegg
 
 
 class TestGroup(unittest.TestCase):
-    def test__filter_fluxes(self):
-        # CASE 1: Regular Group
+    def test___init__(self):
+        # CASE 1: regular __init__
+        test_group = pt.Pathway(id="test_group")
+        self.assertIn(member="solution", container=dir(test_group))
+        self.assertIn(member="members", container=dir(test_group))
+        self.assertIn(member="kind", container=dir(test_group))
+        self.assertIn(member="id", container=dir(test_group))
+        self.assertIn(member="KIND_TYPES", container=dir(test_group))
+
+    def test__filter(self):
+        # Configuration
         test_model = textbook_kegg.copy()
-        # test_model.groups = gr.Pathway
         members = DictList()
         members.union(
             iterable=[
@@ -20,18 +28,38 @@ class TestGroup(unittest.TestCase):
         )
         test_group = pt.Pathway(id="test_group", members=members)
         test_model.add_groups([test_group])
-        test_solution = test_model.optimize()
-        list(
-            test_model.groups.get_by_id("test_group")._filter_fluxes(
-                solution=test_solution
-            )
+        # CASE 1: Fluxes
+        test_serie = test_model.groups.get_by_id("test_group")._filter(
+            solution=test_model.optimize(), attribute="fluxes"
         )
+        self.assertEqual(first=test_serie["ADK1"], second=0)
+        # CASE 2: Shadows prices
+        test_serie = test_model.groups.get_by_id("test_group")._filter(
+            solution=test_model.optimize(), attribute="shadow_prices"
+        )
+        self.assertEqual(first=test_serie["ADK1"], second=0)
 
-        # gr._filter_fluxes(
-        #     solution=test_solution,
-        #     group=test_model.groups.get_by_id("test_group"),
-        # )
-        pass
+    def test__solution(self):
+        # Configuration
+        test_model = textbook_kegg.copy()
+        members = DictList()
+        members.union(
+            iterable=[test_model.reactions.ADK1, test_model.reactions.ACKr]
+        )
+        test_group = pt.Pathway(id="test_group", members=members)
+        test_model.add_groups([test_group])
+
+        test_solution = test_model.groups.get_by_id("test_group").solution(
+            solution=test_model.optimize()
+        )
+        # CASE 1: Regular fluxes
+        for attribute in (
+            test_solution.fluxes,
+            test_solution.reduced_costs,
+            test_solution.shadow_prices,
+        ):
+            self.assertTrue(expr="ADK1" in attribute)
+            self.assertTrue(expr="ACKr" in attribute)
 
 
 if __name__ == "__main__":
