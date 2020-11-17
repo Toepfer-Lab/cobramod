a588ef4b8acfd9c88a1eef71a3f4063df18024c8
Stefano C <cruz@ipk-gatersleben.de>
2020-11-17 14:37:00 +0100

    UPDATED: setup.py now add data information inside src directory. FIXED: path for test_models corrected. No CWD.

--- setup.py
+++ setup.py
@@ -3,12 +3,11 @@ from setuptools import setup, find_packages
 setup(
     name="cobramod",
     version="0.0.1a0",
-    packages=find_packages(where='src'),
-    package_dir={'': 'src'},
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    package_data={"": ["data/*"]},
     test_suite="tests",
-    python_requires='>=3.7.4',
-    install_requires=[
-        "cobra>=0.18.1",
-        "requests>=2.24.0"],
-    url="https://gitlab.com/camborda.s/cobramod"
+    python_requires=">=3.7.4",
+    install_requires=["cobra>=0.18.1", "requests>=2.24.0"],
+    url="https://gitlab.com/camborda.s/cobramod",
 )
--- src/cobramod/test.py
+++ src/cobramod/test.py
@@ -1,10 +1,12 @@
 from pathlib import Path
+
 from cobra.io import read_sbml_model
 from cobra.test import create_test_model
 
-data_dir = Path.cwd().joinpath("src").joinpath("cobramod").joinpath("data")
+data_dir = Path(__file__).parents[0].joinpath("data")
 
 textbook = create_test_model(model_name="textbook")
+
 textbook_biocyc = read_sbml_model(
     filename=str(data_dir.joinpath("textbook_biocyc.sbml"))
 )
