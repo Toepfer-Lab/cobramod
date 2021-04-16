6f2e8f597605af21076348b71a3dacdca6ff8840
Stefano C <cruz@ipk-gatersleben.de>
2021-04-16 17:24:12 +0200

    UPDATED: version bumped to 0.3.2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="cobramod",
-    version="0.3.1",
+    version="0.3.2",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     package_data={"": ["data/*"]},
