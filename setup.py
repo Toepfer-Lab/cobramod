7d1cbdfa11a25fa1128171e6091e0770fad7051b
Stefano C <cruz@ipk-gatersleben.de>
2021-04-16 13:08:20 +0200

    FIXED: version increased to 0.3.1

--- setup.py
+++ setup.py
@@ -7,12 +7,17 @@ from setuptools import setup, find_packages
 
 setup(
     name="cobramod",
-    version="0.3.0",
+    version="0.3.1",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     package_data={"": ["data/*"]},
     test_suite="tests",
     python_requires=">=3.7.4",
-    install_requires=["cobra>=0.18.1", "requests>=2.24.0", "Escher>=1.7.3", "openpyxl>=3.0.7"],
+    install_requires=[
+        "cobra>=0.18.1",
+        "requests>=2.24.0",
+        "Escher>=1.7.3",
+        "openpyxl>=3.0.7",
+    ],
     url="https://github.com/Toepfer-Lab/cobramod",
 )
