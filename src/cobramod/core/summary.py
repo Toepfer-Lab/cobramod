f41c9c607412ebc518bd71280a48bb4bd6eac9f9
Jan-Niklas Weder <Jan-Niklas.Weder@live.de>
2021-06-26 12:27:09 +0200

    UPDATED: Style adjustment according to black

--- src/cobramod/core/summary.py
+++ src/cobramod/core/summary.py
@@ -319,7 +319,9 @@ class DataModel:
 
 
 def summary(
-    model: Model, original: DataModel, filename: Path = None,
+    model: Model,
+    original: DataModel,
+    filename: Path = None,
 ):
     """
     Produces the short summary and another one in the defined format.
