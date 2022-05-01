9ba6c1ae66e01b8f0a2b8b67525df21209394c67
Jan-Niklas Weder <Jan-Niklas.Weder@live.de>
2022-05-01 21:29:06 +0200

    Updated loading of the Textbook model.
    Due to the changes in COBRApy regarding the availability of the textbook model in version 0.25.0, the creation/loading of the textbook model has been adjusted.

--- src/cobramod/test.py
+++ src/cobramod/test.py
@@ -5,15 +5,26 @@ This module creates the textbook models for testing. These are based on the
 original model "e_coli_core" from BiGG database, which are also included in
 COBRApy.
 """
+import gzip
+
+from importlib_resources import open_binary, files, as_file
 from pathlib import Path
 
-from cobra.io import read_sbml_model
-from cobra.test import create_test_model
+from cobra.io import read_sbml_model, validate_sbml_model
+import cobra.data
 
 # This is the directory where the textbook models are located.
 data_dir = Path(__file__).resolve().parent.joinpath("data")
 # These are the three posibilities
-textbook = create_test_model(model_name="textbook")
+
+try:
+    textbook
+except NameError:
+    textbook_raw = files(cobra.data).joinpath("textbook.xml")
+    with as_file(textbook_raw) as textbookXML:
+        textbook = read_sbml_model(str(textbookXML))
+
+
 textbook_biocyc = read_sbml_model(
     filename=str(data_dir.joinpath("textbook_biocyc.sbml"))
 )
