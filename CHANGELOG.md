2d29916f821cae5a98f14ab03a2b64a738e596a4
Stefano C. <scambord@uni-koeln.de>
2024-02-19 11:24:11 +0100

    Tox now runs the test suite and not GH UPDATE: .yml for test and publish updated with more recents actions NEW: changelog UPDATE: environment.yml includes all deps for developement UPDATE: in pyproject.toml, updating dependencies NEW: logging information is more human-friendly and includes colors

---
+++ CHANGELOG.md
@@ -0,0 +1,28 @@
+# Changelog
+
+## [Unreleased]
+
+## [1.2.0-dev]
+
+### Added
+
+- Memote example files added to docs
+- curation logs are now separated by date
+
+### Changed
+
+- Escher is an optional dependency. Pip installation files reflect this change
+- Pre-hooks uses now ruff instead of flake8 and black. This is reflected also
+in the dev-environment
+- Jupyter files for the documentation reflect current changes
+- Test data updated (15.01.24)
+- Dropped support for python 3.8 and 3.9
+- Logging format is more human-friendly
+- Dropped support of mypy. Black is replaced with ruff
+- New pyproject.toml
+
+### Removed
+
+- Deprecated config files
+- Deprecated guide
+- Deprecation of SolCyc. Database is not responding
--- src/cobramod/debug.py
+++ src/cobramod/debug.py
@@ -8,16 +8,43 @@ The default level is set to INFO.
 
 The name of logger variable is `debug_log`
 """
-from logging import INFO, FileHandler, Formatter, getLogger
+import datetime as dt
+import logging
+from pathlib import Path
 
-# Creating corresponding Logs
-# Format
-debug_formatter = Formatter("%(asctime)s %(levelname)s %(message)s")
-# Handler
-debug_handler = FileHandler("debug.log", mode="a+")
-debug_handler.setFormatter(debug_formatter)
+import colorlog
+
 # Log
-debug_log = getLogger("debug_log")
-debug_log.setLevel(INFO)
-# GenLog.ad
+debug_log = logging.getLogger("debug_log")
+debug_log.setLevel(logging.INFO)
+
+format_str = "[%(asctime)s] %(log_color)s%(levelname)s %(message)s"
+TIME_STR = "%H:%M:%S"
+
+colors = {
+    "DEBUG": "white",
+    "INFO": "cyan",
+    "WARNING": "yellow",
+    "ERROR": "red",
+    "CRITICAL": "purple",
+}
+formatter = colorlog.ColoredFormatter(format_str, TIME_STR, log_colors=colors)
+
+# In direct stream
+stream_handler = logging.StreamHandler()
+stream_handler.setFormatter(formatter)
+
+debug_log.addHandler(stream_handler)
+
+format_str = "[%(asctime)s] %(levelname)s %(message)s"
+# File logs
+formatter = logging.Formatter(format_str, TIME_STR)
+
+log_dir = Path("logs").absolute()
+log_dir.mkdir(exist_ok=True)
+log_path = log_dir.joinpath(f"cobramod_{dt.date.today().strftime('%Y%m%d')}")
+
+debug_handler = logging.FileHandler(log_path, mode="a+")
+debug_handler.setFormatter(formatter)
+
 debug_log.addHandler(debug_handler)
