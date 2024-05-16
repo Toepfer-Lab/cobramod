c1651049e4df39a851e464ffebc9aebcc25d2de1
Jan-Niklas Weder <Jan-Niklas.Weder@live.de>
2024-05-16 16:33:50 +0200

    Format

--- tests/test_test.py
+++ tests/test_test.py
@@ -13,13 +13,17 @@ import cobramod
 class ModuleTest(unittest.TestCase):
     def test_directory(self):
         self.assertTrue(
-                resources.files(cobra).joinpath("data/textbook.xml.gz").is_file()
+            resources.files(cobra).joinpath("data/textbook.xml.gz").is_file()
         )
         self.assertTrue(
-            resources.files(cobramod).joinpath("data/textbook_biocyc.sbml").is_file()
+            resources.files(cobramod)
+            .joinpath("data/textbook_biocyc.sbml")
+            .is_file()
         )
         self.assertTrue(
-            resources.files(cobramod).joinpath("data/textbook_kegg.sbml").is_file()
+            resources.files(cobramod)
+            .joinpath("data/textbook_kegg.sbml")
+            .is_file()
         )
 
 
