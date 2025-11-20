7a12cf62b50b9612de5a92405e083950f2dbcb63
Jan-Niklas Weder <jweder@uni-koeln.de>
2025-11-20 13:14:41 +0100

    Added Bigg functionality
    Added query functions for Bigg and made the BioCyc query functions compliant with the abstract class.

--- src/cobramod/dbwalker/BioCyc.py
+++ src/cobramod/dbwalker/BioCyc.py
@@ -1,11 +1,12 @@
 import urllib
 
 import requests
-from typing import Optional, Dict, Any
+from typing import Optional, Dict, Any, Union, Tuple, overload
 
 import xml.etree.ElementTree as ET
 import logging
 
+from cobramod.dbwalker.DataBase import Database
 from cobramod.dbwalker.dataclasses import GenerellIdentifiers
 import cobramod
 
@@ -21,157 +22,180 @@ logger.addHandler(console_handler)
 
 biocycTier1DB = ["Meta","ECOLI", "HUMAN", "ARA", "YEAST"]
 
-def get_compound_info_by_biocyc_id(biocyc_id: str, db: str = "META") -> GenerellIdentifiers:
-    """
-    Get InChI, InChI Key, and SMILES for a compound using BioCyc ID.
-    
-    Args:
-        biocyc_id: BioCyc compound ID (e.g., 'CPD-123')
-        db: BioCyc database name (default: 'META' for MetaCyc)
-    
-    Returns:
-        Dictionary with 'inchi', 'inchi_key', and 'smiles' keys
-    """
+class BioCyc(Database):
 
-    url = f"https://websvc.biocyc.org/getxml"
-    
-    params = {
+    @overload
+    def getGenerellIdentifier(self, dbIdentifier: str) -> GenerellIdentifiers:
+        ...
+
+    @overload
+    def getGenerellIdentifier(self, dbIdentifier: str, BioCycSubDB: str) -> GenerellIdentifiers:
+        ...
+
+    def getGenerellIdentifier(self, dbIdentifier: str, **kwargs) -> GenerellIdentifiers:
+        """
+        Get InChI, InChI Key, and SMILES for a compound using BioCyc ID.
+
+        Args:
+            biocyc_id: BioCyc compound ID (e.g., 'CPD-123')
+            db: BioCyc database name (default: 'META' for MetaCyc)
+
+        Returns:
+            Dictionary with 'inchi', 'inchi_key', and 'smiles' keys
+        """
+
+
+        BioCycSubDB = kwargs.get('BioCycSubDB')
+
+        if BioCycSubDB is not None:
+            db, biocyc_id = dbIdentifier, BioCycSubDB
+            pass
+        else:
+            db, biocyc_id = dbIdentifier.split(":")
+
+        url = f"https://websvc.biocyc.org/getxml"
+
+        params = {
             'id': f"{db}:{biocyc_id}"
-    }
+        }
 
-    # BioCyc cuts the SMILES string short otherwise
-    headers = {
-        "User-Agent": "Mozilla/5.0",
-        "Accept": "application/xml"
-    }
+        # BioCyc cuts the SMILES string short otherwise
+        headers = {
+            "User-Agent": "Mozilla/5.0",
+            "Accept": "application/xml"
+        }
 
-    logger.debug(f"Querying BioCyc for compound {biocyc_id} in database {db}")
-    logger.debug(f"Request URL: {url}")
-    logger.debug(f"Request parameters: {params}")
+        logger.debug(f"Querying BioCyc for compound {biocyc_id} in database {db}")
+        logger.debug(f"Request URL: {url}")
+        logger.debug(f"Request parameters: {params}")
 
-    result = GenerellIdentifiers()
-    try:
-        cSettings = cobramod.Settings()
-        session = cSettings._biocycSession
-        response = session.get(url, params=params,headers=headers, timeout=30)
-        response.raise_for_status()
+        result = GenerellIdentifiers()
+        try:
+            cSettings = cobramod.Settings()
+            session = cSettings._biocycSession
+            response = session.get(url, params=params, headers=headers, timeout=30)
+            response.raise_for_status()
 
-        if cSettings.autoOpenCloseBioCycSession:
+            if cSettings.autoOpenCloseBioCycSession:
+                cSettings._closeBiocycSession()
+
+            logger.debug(f"Response status code: {response.status_code}")
+
+            # Check if BioCyc is asking for account creation
+            if response.text and isinstance(response.text, str) and "<title>Create Account</title>" in response.text:
+                logger.warning("BioCyc is requesting account creation.")
+                if not cSettings.BioCycLoggedIn:
+                    logger.error("BioCyc account information are not provided can't login.")
+                return result
+
+            # Parse XML response
+            root = ET.fromstring(response.content)
+
+            # Extract SMILES from XML
+            smiles_element = root.find(".//string[@title='smiles']")
+            if smiles_element is not None:
+                result.smiles = smiles_element.text.strip()
+
+            # Extract InChI from XML
+            inchi_element = root.find(".//inchi[@datatype='string']")
+            if inchi_element is not None:
+                inchi = inchi_element.text.strip()
+                if inchi.startswith("InChI="):
+                    result.inchi = inchi[6:]
+                else:
+                    result.inchi = inchi
+
+            # Extract InChI Key from XML
+            inchikey_element = root.find(".//inchi-key[@datatype='string']")
+            if inchikey_element is not None:
+                inchikey = inchikey_element.text.strip()
+
+                if inchikey.startswith("InChIKey="):
+                    result.inchi_key = inchikey[9:]
+                else:
+                    result.inchi_key = inchikey
+
+        except requests.RequestException as e:
+            logger.error(f"Error fetching data from BioCyc: {e}")
+        except ET.ParseError as e:
+            logger.error(f"Error parsing XML response: {e}")
+
+        logger.debug(f"Final result: {result}")
+        return result
+
+    def getDBIdentifierFromSmiles(self, smiles: Union[str, GenerellIdentifiers]) -> Optional[str]:
+        """
+        Convert SMILES to BioCyc identifiers.
+
+        Args:
+            smiles: SMILES string of the compound
+
+        Returns:
+            GenerellIdentifiers with BioCyc ID if found, otherwise empty
+        """
+
+        try:
+            encoded_smiles = urllib.parse.quote(smiles, safe='')
+            url = f"https://websvc.biocyc.org/{orgid}/smiles-search?smiles={encoded_smiles}&exact=T&fmt=json"
+
+            logger.info(f"Requesting BioCyc for SMILES: {smiles}")
+            logger.debug(f"Request URL: {url}")
+
+            cSettings = cobramod.Settings()
+            session = cSettings._biocycSession
+
+            response = session.get(url, timeout=30)
+            response.raise_for_status()
+
             cSettings._closeBiocycSession()
 
-        logger.debug(f"Response status code: {response.status_code}")
+            data = response.json()
 
-        # Check if BioCyc is asking for account creation
-        if response.text and isinstance(response.text, str) and "<title>Create Account</title>" in response.text:
-            logger.warning("BioCyc is requesting account creation.")
-            if not cSettings.BioCycLoggedIn:
-                logger.error("BioCyc account information are not provided can't login.")
-            return result
+            print(data)
+            biocycID = data["RESULTS"][0]["OBJECT-ID"]
+            return biocycID
 
-        # Parse XML response
-        root = ET.fromstring(response.content)
 
-        
-        # Extract SMILES from XML
-        smiles_element = root.find(".//string[@title='smiles']")
-        if smiles_element is not None:
-            result.smiles = smiles_element.text.strip()
+        except requests.RequestException as e:
+            logger.error(f"Error fetching data from BioCyc: {e}")
+            return None
 
-        # Extract InChI from XML
-        inchi_element = root.find(".//inchi[@datatype='string']")
-        if inchi_element is not None:
-            inchi = inchi_element.text.strip()
-            if inchi.startswith("InChI="):
-                result.inchi = inchi[6:]
-            else:
-                result.inchi = inchi
+    def getDBIdentifierFromInchi(self, smiles: Union[str, GenerellIdentifiers]) -> Optional[str]:
+        """
+            Convert InChI to BioCyc identifiers.
 
-        # Extract InChI Key from XML
-        inchikey_element = root.find(".//inchi-key[@datatype='string']")
-        if inchikey_element is not None:
-            inchikey = inchikey_element.text.strip()
+            Args:
+                inchi: InChI string of the compound
 
-            if inchikey.startswith("InChIKey="):
-                result.inchi_key = inchikey[9:]
-            else:
-                result.inchi_key = inchikey
-        
-    except requests.RequestException as e:
-        logger.error(f"Error fetching data from BioCyc: {e}")
-    except ET.ParseError as e:
-        logger.error(f"Error parsing XML response: {e}")
+            Returns:
+                GenerellIdentifiers with BioCyc ID if found, otherwise empty
+        """
+        try:
+            url = f"https://websvc.biocyc.org/{orgid}/inchi-search?inchi={inchi}&exact=T&fmt=json"
 
-    logger.debug(f"Final result: {result}")
-    return result
+            logger.info(f"Requesting BioCyc for InChI: {inchi}")
 
-def smiles2BioCyc(smiles: str, orgid: str = "META") -> str:
-    """
-    Convert SMILES to BioCyc identifiers.
-    
-    Args:
-        smiles: SMILES string of the compound
-        
-    Returns:
-        GenerellIdentifiers with BioCyc ID if found, otherwise empty
-    """
-    try:
-        encoded_smiles = urllib.parse.quote(smiles, safe='')
-        url = f"https://websvc.biocyc.org/{orgid}/smiles-search?smiles={encoded_smiles}&exact=T&fmt=json"
-        
-        logger.info(f"Requesting BioCyc for SMILES: {smiles}")
-        logger.debug(f"Request URL: {url}")
+            cSettings = cobramod.Settings()
+            session = cSettings._biocycSession
 
-        cSettings = cobramod.Settings()
-        session = cSettings._biocycSession
+            response = session.get(url, timeout=30)
+            response.raise_for_status()
 
-        response = session.get(url, timeout=30)
-        response.raise_for_status()
+            cSettings._closeBiocycSession()
 
-        cSettings._closeBiocycSession()
-        
-        data = response.json()
+            data = response.json()
 
-        print(data)
-        biocycID = data["RESULTS"][0]["OBJECT-ID"]
-        return biocycID
-        
-        
-    except requests.RequestException as e:
-        logger.error(f"Error fetching data from BioCyc: {e}")
-        return None
+            biocycID = data["RESULTS"][0]["OBJECT-ID"]
+            return biocycID
 
-def InChI2BioCyc(inchi: str, orgid: str = "META") -> Optional[str]:
-    """
-    Convert InChI to BioCyc identifiers.
-    
-    Args:
-        inchi: InChI string of the compound
-        
-    Returns:
-        GenerellIdentifiers with BioCyc ID if found, otherwise empty
-    """
-    try:
-        url = f"https://websvc.biocyc.org/{orgid}/inchi-search?inchi={inchi}&exact=T&fmt=json"
-        
-        logger.info(f"Requesting BioCyc for InChI: {inchi}")
 
-        cSettings = cobramod.Settings()
-        session = cSettings._biocycSession
+        except requests.RequestException as e:
+            logger.error(f"Error fetching data from BioCyc: {e}")
+            return None
 
-        response = session.get(url, timeout=30)
-        response.raise_for_status()
+    def getDBIdentifierFromInchiKey(self, smiles: Union[str, GenerellIdentifiers]) -> str:
+        pass
 
-        cSettings._closeBiocycSession()
-        
-        data = response.json()
-
-        biocycID = data["RESULTS"][0]["OBJECT-ID"]
-        return biocycID
-        
-        
-    except requests.RequestException as e:
-        logger.error(f"Error fetching data from BioCyc: {e}")
-        return None
-
-#def Identifiers2BioCyc(identifier: str) -> GenerellIdentifiers:
-    
+    def validateGenerellIdentifiers(self, smiles: Union[str, GenerellIdentifiers]) -> Tuple[
+        GenerellIdentifiers, GenerellIdentifiers]:
+        pass
--- src/cobramod/dbwalker/bigg.py
+++ src/cobramod/dbwalker/bigg.py
@@ -1,7 +1,8 @@
 import requests
 import logging
-from typing import Optional, Dict, Any
+from typing import Optional, Dict, Any, Union, Tuple
 
+from cobramod.dbwalker.DataBase import Database
 from cobramod.dbwalker.dataclasses import GenerellIdentifiers
 
 logger = logging.getLogger("cobramod.DBWalker.Bigg")
@@ -14,62 +15,79 @@ formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(messag
 console_handler.setFormatter(formatter)
 logger.addHandler(console_handler)
 
-def biggID2Identifier(bigg_id: str) -> GenerellIdentifiers:
-    """
-    Convert a BiGG metabolite ID to available general identifiers.
+class Bigg(Database):
+    def getDBIdentifierFromSmiles(self, smiles: Union[str, GenerellIdentifiers]) -> str:
+        pass
 
-    Args:
-        bigg_id: BiGG database metabolite identifier
-        
-    Returns:
-        Dictionary containing inchi, inchi_key, and smiles if available
-    """
-    foundIDs = GenerellIdentifiers(
+    def getDBIdentifierFromInchi(self, smiles: Union[str, GenerellIdentifiers]) -> str:
+        pass
+
+    def getDBIdentifierFromInchiKey(self, smiles: Union[str, GenerellIdentifiers]) -> str:
+        pass
+
+    def validateGenerellIdentifiers(self, smiles: Union[str, GenerellIdentifiers]) -> Tuple[
+        GenerellIdentifiers, GenerellIdentifiers]:
+        pass
+
+    def __init__(self):
+        super().__init__()
+
+    def getGenerellIdentifier(self, dbIdentifier:str) -> GenerellIdentifiers:
+        """
+        Convert a BiGG metabolite ID to available general identifiers.
+
+        Args:
+            bigg_id: BiGG database metabolite identifier
+
+        Returns:
+            Dictionary containing inchi, inchi_key, and smiles if available
+        """
+        foundIDs = GenerellIdentifiers(
             inchi=None,
             inchi_key=None,
             smiles=None
         )
 
-    try:
-        url = f"http://bigg.ucsd.edu/api/v2/universal/metabolites/{bigg_id}"
-        
-        logger.info(
-            f"Requesting data from Bigg for metabolite with id {bigg_id}"
-        )
-        logger.debug(
-            f"Request URL: {url}"
-        )
+        try:
+            url = f"http://bigg.ucsd.edu/api/v2/universal/metabolites/{dbIdentifier}"
 
-        response = requests.get(url)
-        response.raise_for_status()
-    except requests.RequestException as e:
-        logger.error(f"Request to BiGG API failed: {e}")
+            logger.info(
+                f"Requesting data from Bigg for metabolite with id {dbIdentifier}"
+            )
+            logger.debug(
+                f"Request URL: {url}"
+            )
+
+            response = requests.get(url)
+            response.raise_for_status()
+        except requests.RequestException as e:
+            logger.error(f"Request to BiGG API failed: {e}")
+            return foundIDs
+
+        data = response.json()
+
+        try:
+            data = data['database_links']
+        except KeyError:
+            logger.warning("No database links found in BiGG response")
+            return foundIDs
+
+        try:
+            foundIDs.inchi = data['InChI'][0]['id']
+            logger.debug(f"Found InChI: {foundIDs.inchi}")
+        except (KeyError, IndexError):
+            logger.debug("No InChI found in BiGG response")
+
+        try:
+            foundIDs.inchi_key = data['InChI Key'][0]['id']
+            logger.debug(f"Found InChI Key: {foundIDs.inchi_key}")
+        except (KeyError, IndexError):
+            logger.debug("No InChI Key found in BiGG response")
+
+        try:
+            foundIDs.smiles = data['SMILES'][0]['id']
+            logger.debug(f"Found SMILES: {foundIDs.smiles}")
+        except (KeyError, IndexError):
+            logger.debug("No SMILES found in BiGG response")
+
         return foundIDs
-        
-    data = response.json()
-
-    try:
-        data = data['database_links']
-    except KeyError:
-        logger.warning("No database links found in BiGG response")
-        return foundIDs
-    
-    try:
-        foundIDs.inchi = data['InChI'][0]['id']
-        logger.debug(f"Found InChI: {foundIDs.inchi}")
-    except (KeyError, IndexError):
-        logger.debug("No InChI found in BiGG response")
-
-    try:
-        foundIDs.inchi_key = data['InChI Key'][0]['id']
-        logger.debug(f"Found InChI Key: {foundIDs.inchi_key}")
-    except (KeyError, IndexError):
-        logger.debug("No InChI Key found in BiGG response")
-
-    try:
-        foundIDs.smiles = data['SMILES'][0]['id']
-        logger.debug(f"Found SMILES: {foundIDs.smiles}")
-    except (KeyError, IndexError):
-        logger.debug("No SMILES found in BiGG response")
-
-    return foundIDs
